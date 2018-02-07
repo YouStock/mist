08c5d414ba3657baa513e14b69a318e31804234d
Ben Burnett <ben@youstock.io>
2018-02-07 15:58:08 -0700

    update clientbinaries url and regex

--- modules/clientBinaryManager.js
+++ modules/clientBinaryManager.js
@@ -11,12 +11,11 @@ const EventEmitter = require('events').EventEmitter;
 
 const log = require('./utils/logger').create('ClientBinaryManager');
 
-
-// should be       'https://raw.githubusercontent.com/ethereum/mist/master/clientBinaries.json'
-const BINARY_URL = 'https://raw.githubusercontent.com/ethereum/mist/master/clientBinaries.json';
+// should be        https://raw.githubusercontent.com/YouStock/deskwallet/master/clientBinaries.json
+const BINARY_URL = 'https://raw.githubusercontent.com/YouStock/deskwallet/master/clientBinaries.json';
 
 const ALLOWED_DOWNLOAD_URLS_REGEX =
-    /^https:\/\/(?:(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?\.)?ethereum\.org\/|gethstore\.blob\.core\.windows\.net\/|bintray\.com\/artifact\/download\/karalabe\/ethereum\/)(?:.+)/;  // eslint-disable-line max-len
+    /^https:\/\/github\.com\/YouStock\/go-aura\/releases\/download\/(?:.+)/;  // eslint-disable-line max-len
 
 class Manager extends EventEmitter {
     constructor() {
